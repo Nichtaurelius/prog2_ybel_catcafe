32bb493c6e7d019f7a755051ba932c5c1ee2f4b9
Aurelius <aurxlius@protonmail.ch>
2026-05-10 17:46:10 +0200

    Format code with spotless

--- src/test/java/catcafe/CatCafeTest.java
+++ src/test/java/catcafe/CatCafeTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 /** Tests for {@link CatCafe}. */
